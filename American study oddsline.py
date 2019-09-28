adb906037fb3adf90df49f7c479d9ce27df84cd5
ItsClaudiPie <45623055+ItsClaudiPie@users.noreply.github.com>
2019-09-28 18:17:03 +0200

    Add files via upload

---
+++ American study oddsline.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  8 22:05:03 2019
+
+@author: User
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from math import e
+
+intercept = -0.6621
+est = 0.2416
+odds = list()
+
+
+for i in range(3): 
+    odds.append( e **(intercept + est*(i-2)))
+    
+actual_odds = [ 0.2 , 0.3 ,0.4,0.5, 0.6, 0.85]
+ 
+
+x = [-2,-1,0,1,2]
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+plt.title('Estimating Odds Using Logistic Regression')
+
+ax1.scatter(x, odds, s=10, c='b', marker="s", label='estimated odds')
+ax1.scatter(x,actual_odds, s=10, c='r', marker="o", label='actual odds')
+ax1.plot(x, odds)
+ax1.plot(x,actual_odds)
+plt.legend(loc='upper left');
+
+plt.ylim(0, 2.5)
+plt.xlabel("Qualification")
+plt.ylabel("Odds")
+plt.savefig(r'C:\Users\User\Desktop\oddsline.png')
+
+
+plt.show()
+
+
+
